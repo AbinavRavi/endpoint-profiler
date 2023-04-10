3c1f9b4d21d13e7e7d8da927e2b046336873c98d
AbinavRavi <subramathreya@gmail.com>
2023-04-10 20:39:12 +0200

    added profiler decorator pattern need to make it a proper package

---
+++ profiler.py
@@ -0,0 +1,11 @@
+import cProfile
+
+def profile_decorator(func):
+    def wrapper(*args, **kwargs):
+        profiler = cProfile.Profile()
+        profiler.enable()
+        result = func(*args, **kwargs)
+        profiler.disable()
+        profiler.print_stats()
+        return result
+    return wrapper
