aa47effd511cd107185f6669b46f35a0920d04c5
AbinavRavi <subramathreya@gmail.com>
2023-04-13 08:17:13 +0200

    added logging component needs to be tested on an endpoint

--- profiler.py
+++ profiler.py
@@ -1,13 +1,17 @@
 import cProfile
 import pstats
+import logging
+
 
 def profiler(func):
     def wrapper(*args, **kwargs):
+        logger = logging.getLogger()
+        logging.basicConfig(level = logging.DEBUG)
         profiler = cProfile.Profile()
         profiler.enable()
         result = func(*args, **kwargs)
         profiler.disable()
-        profiler.print_stats()
+        logging.info(profiler.print_stats())
         # stats = pstats.Stats(profiler).sort_stats('tottime')
         # stats.dump_stats('/profile.csv')
         # stats.print_stats()
