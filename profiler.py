1c529679bfdbc96cb1343c33246beabe60f8c29d
AbinavRavi <subramathreya@gmail.com>
2023-04-15 10:48:08 +0200

    added code whisperer to gitignore

--- profiler.py
+++ profiler.py
@@ -11,7 +11,7 @@ def profiler(func):
         profiler.enable()
         result = func(*args, **kwargs)
         profiler.disable()
-        logging.info(profiler.print_stats())
+        logger.info(profiler.print_stats())
         # stats = pstats.Stats(profiler).sort_stats('tottime')
         # stats.dump_stats('/profile.csv')
         # stats.print_stats()
